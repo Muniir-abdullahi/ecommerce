345b10b2df3d488c0ee295064bfb13ef4bb66308
Diinbadane <muniir669@gmail.com>
2022-09-04 22:49:25 +0300

    Update [slug].js

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -22,11 +22,11 @@ const ProductDetails = ({ product, products }) => {
       <div className="product-detail-container">
         <div>
           <div className="image-container">
-            <img src={urlFor(image && image[index])} className="product-detail-image" />
+            <img alt = 'product image' src={urlFor(image && image[index])} className="product-detail-image" />
           </div>
           <div className="small-images-container">
             {image?.map((item, i) => (
-              <img 
+              <img alt = 'product imges'
                 key={i}
                 src={urlFor(item)}
                 className={i === index ? 'small-image selected-image' : 'small-image'}
